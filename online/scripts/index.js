1922e6d3a1799414fafa633041be53aa5d0152b6
Muzammil Khaja <muzammilkm@gmail.com>
2022-12-24 21:37:35 +0530

    removed confirmation secret from upload, enforcing confirmation secret in download

--- online/scripts/index.js
+++ online/scripts/index.js
@@ -280,9 +280,11 @@
 
         view.init = function () {
             var s = this;
-            s.secretList = $("#secret-list tbody");
+            s.secretDownloadList = $("#secret-download-list tbody");
+            s.secretUploadList = $("#secret-upload-list tbody");
             s.noRowMessageTml = _.template($("#no-row-message-tml").html());
-            s.secretRowTml = _.template($("#secret-row-tml").html());
+            s.secretDownloadRowTml = _.template($("#secret-download-row-tml").html());
+            s.secretUploadRowTml = _.template($("#secret-upload-row-tml").html());
 
             return s;
         };
@@ -290,21 +292,27 @@
         view.render = function () {
             var s = this,
                 html = "",
-                secretList = model.getSecretList();
+                secretList = model.getSecretList(),
+                secretRowTml = view.getSecretListRowTml(),
+                secretListView = view.getSecretListView();
+
+            s.secretDownloadList.parent().hide();
+            s.secretUploadList.parent().hide();
             if (secretList.length === 0) {
                 html = "<tr class='no-data'><td colspan='4'>" + s.noRowMessageTml() + "</td></tr>";
             }
             else {
                 secretList.forEach((secret, index) => {
-                    html += s.secretRowTml({ ...secret, index });
+                    secret.confirmSecret = '';
+                    html += secretRowTml({ ...secret, index });
                 });
             }
-            s.secretList.html(html);
+            secretListView.html(html).parent().show();
             $(".delete-secret").click(e => {
                 var elem = $(e.target).closest("tr");
                 vm.removeSecret(elem.data("index"));
             });
-            s.secretList.find("input").change(e => {
+            secretListView.find("input").change(e => {
                 view.validate();
             });
             return s;
@@ -312,8 +320,9 @@
 
         view.get = function () {
             var s = this,
-                secretList = [];
-            s.secretList.children('tr').each((i, trElem) => {
+                secretList = [],
+                secretListView = view.getSecretListView();
+            secretListView.children('tr').each((i, trElem) => {
                 var tr = $(trElem);
                 if (!tr.hasClass("no-data")) {
                     secretList.push({ secret: tr.find('.secret').val(), confirmSecret: tr.find('.confirm-secret').val() })
@@ -324,8 +333,13 @@
 
         view.validate = function () {
             var s = this,
-                isValid = true;
-            s.secretList.children('tr').each((i, trElem) => {
+                isValid = true,
+                secretListView = view.getSecretListView();
+            if (secretListView === s.secretUploadList) {
+                return isValid;
+            }
+
+            secretListView.children('tr').each((i, trElem) => {
                 var tr = $(trElem);
                 if (tr.hasClass("no-data")) {
                     isValid = false;
@@ -345,6 +359,18 @@
             });
             return isValid;
         };
+
+        view.getSecretListView = function () {
+            var s = this,
+                modalType = model.getSecretModalType();
+            return modalType == 'upload' ? s.secretUploadList : s.secretDownloadList;
+        };
+
+        view.getSecretListRowTml = function () {
+            var s = this,
+                modalType = model.getSecretModalType();
+            return modalType == 'upload' ? s.secretUploadRowTml : s.secretDownloadRowTml;
+        }
 
         return view.init();
     }
